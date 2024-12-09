5781c4a7bcef1a34fcded86fe05a699bdd75aa55
huangyong <braveryhuang@126.com>
2024-12-09 09:26:43 +0800

    模糊查询（concat 或 "%"#{username}"%"）

---
+++ MyBatis/mybatis_demo2/src/main/java/com/huangy/mybatis/mapper/SQLMapper.java
@@ -0,0 +1,14 @@
+package com.huangy.mybatis.mapper;
+
+import com.huangy.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SQLMapper {
+
+    /**
+     * 根据用户名模糊查询用户信息
+     */
+    List<User> getUserByLike(@Param("username") String username);
+}
---
+++ MyBatis/mybatis_demo2/src/test/java/com/huangy/mybatis/test/SQLMapperTest.java
@@ -0,0 +1,22 @@
+package com.huangy.mybatis.test;
+
+import com.huangy.mybatis.mapper.SQLMapper;
+import com.huangy.mybatis.mapper.SelectMapper;
+import com.huangy.mybatis.pojo.User;
+import com.huangy.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.jdbc.SQL;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class SQLMapperTest {
+
+    @Test
+    public void testGetUserByLike() {
+        SqlSession  sqlSession = SqlSessionUtils.getSqlSession();
+        SQLMapper mapper = sqlSession.getMapper(SQLMapper.class);
+        List<User> list = mapper.getUserByLike("a");
+        System.out.println(list);
+    }
+}
